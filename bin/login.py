bfa2f6d93aa6bc9fa9bfa810d10225359167b31b
Jack Thomasson <4302889+jkt628@users.noreply.github.com>
2026-02-28 07:06:58 -0500

    fix login

--- bin/login.py
+++ bin/login.py
@@ -7,7 +7,7 @@ from franklinwh import TokenFetcher
 
 
 async def main():
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         print(f"Usage: {sys.argv[0]} email password")
         sys.exit(1)
     token = await TokenFetcher.login(sys.argv[1], sys.argv[2])
